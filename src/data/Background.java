9e8129e5d5562ccc35faaec7f2fd0937cb47a061
andrewk6 <andrew.knox112@gmail.com>
2025-09-03 10:21:06 -0400

    Prep for next Databases
    Baseline for Background

---
+++ src/data/Background.java
@@ -0,0 +1,16 @@
+package data;
+
+import java.io.Serializable;
+
+import javax.swing.text.StyledDocument;
+
+public class Background implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1895794921904291263L;
+	
+	public String name;
+	public StyledDocument desc;
+}
