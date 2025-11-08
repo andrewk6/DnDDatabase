51de1042fa8768e702ef93e155a6d58586761dd3
andrewk6 <andrew.knox112@gmail.com>
2025-11-08 03:17:32 -0500

    More Enums Added with To Strings

--- src/data/players/classes/DnDClass.java
+++ src/data/players/classes/DnDClass.java
@@ -21,7 +21,20 @@ public class DnDClass implements Serializable, SourceProvider
 	private static final long serialVersionUID = 7913947562880195068L;
 	
 	public enum HitDiceType {d12, d10, d8, d6};
-	public enum WeaponProficiency {Simple, All, SimpleMartialFinesseLight};
+	public enum WeaponProficiency {
+		Simpl("Simple Weapons"), 
+		All("Simple and Martial Weapons"), 
+		SimpleMartialFinesseLight("Simple Weapons and Finesse/Light Martial Weapons");
+		
+		private String desc;
+		WeaponProficiency(String desc){
+			this.desc = desc;
+		}
+		
+		public String toString() {
+			return desc;
+		}
+	};
 	
 	public HashMap<String, ClassAbility> abilities; 
 	public HashMap<String, Subclass> subclasses;
